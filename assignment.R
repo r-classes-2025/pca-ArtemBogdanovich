a3ac0db36d70ccabfc2e3601037526877ed9c90f
ArtemBogdanovich <165372574+ArtemBogdanovich@users.noreply.github.com>
2026-01-30 18:32:53 +0300

    Update assignment.R

--- assignment.R
+++ assignment.R
@@ -1,5 +1,5 @@
 library(friends)
-library(tidyverse)
+library(tidyverse) 
 library(tidytext)
 library(factoextra)
 
@@ -11,33 +11,26 @@ top_speakers <- friends %>%
 friends_tokens <- friends %>%
   filter(speaker %in% top_speakers) %>%
   unnest_tokens(word, text) %>%
-  mutate(word = str_remove_all(word, "\\d")) %>%
+  mutate(word = str_remove_all(word, "\\d+")) %>%
   filter(word != "") %>%
   select(speaker, word)
 
 friends_tf <- friends_tokens %>%
-  count(speaker, word, sort = TRUE) %>%
+  count(speaker, word) %>%
   group_by(speaker) %>%
-  arrange(desc(n)) %>%
-  slice_head(n = 500) %>%
   mutate(tf = n / sum(n)) %>%
-  ungroup()
+  slice_max(n, n = 500) %>%  # топ-500 слов на персонажа
+  ungroup() %>%
+  select(speaker, word, tf)
 
 friends_tf_wide <- friends_tf %>%
-  select(speaker, word, tf) %>%
   pivot_wider(names_from = word, values_from = tf, values_fill = 0) %>%
   column_to_rownames("speaker")
 
 set.seed(123)
 km.out <- kmeans(scale(friends_tf_wide), centers = 3, nstart = 20)
 
-pca_fit <- prcomp(scale(friends_tf_wide), center = TRUE, scale. = TRUE)
-
-q <- fviz_pca_biplot(pca_fit,
-                     label = "var",
-                     habillage = km.out$cluster,
-                     geom.ind = "text",
-                     select.var = list(cos2 = 20))
+pca_fit <- prcomp(friends_tf_wide, center = TRUE, scale. = TRUE)
 
 
 
