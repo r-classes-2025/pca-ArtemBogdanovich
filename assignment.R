527db5587506f15c14631224598bbce25dbabff7
ArtemBogdanovich <165372574+ArtemBogdanovich@users.noreply.github.com>
2026-01-30 17:52:01 +0300

    Update assignment.R

--- assignment.R
+++ assignment.R
@@ -1,50 +1,56 @@
+
 # установите и загрузите пакеты
 library(friends)
 library(tidyverse)
 library(tidytext)
-library(factoextra) 
+library(factoextra)
 
+# 1. отберите 6 главных персонажей по количеству реплик
+top_speakers <- friends %>%
+  count(speaker, sort = TRUE) %>%
+  slice_head(n = 6) %>%
+  pull(speaker)
 
-# 1. отберите 6 главных персонажей (по количеству реплик)
-# сохраните как символьный вектор
-top_speakers <- friends |> 
-  # ваш код здесь
-  
-# 2. отфильтруйте топ-спикеров, 
-# токенизируйте их реплики, удалите из них цифры
-# столбец с токенами должен называться word
-# оставьте только столбцы speaker, word
-friends_tokens <- friends |> 
-  # ваш код здесь
+# 2. отфильтруйте топ-спикеров, токенизируйте их реплики, удалите цифры
+friends_tokens <- friends %>%
+  filter(speaker %in% top_speakers) %>%
+  unnest_tokens(word, text) %>%        # создаем столбец word
+  mutate(word = str_remove_all(word, "\\d")) %>%  # удаляем цифры
+  select(speaker, word)
 
 # 3. отберите по 500 самых частотных слов для каждого персонажа
-# посчитайте относительные частотности для слов
-friends_tf <- friends_tokens |>
-  # ваш код здесь
+# и посчитайте относительные частотности
+friends_tf <- friends_tokens %>%
+  count(speaker, word, sort = TRUE) %>%
+  group_by(speaker) %>%
+  slice_max(n, n = 500) %>%           # топ-500 слов на персонажа
+  ungroup() %>%
+  group_by(speaker) %>%
+  mutate(rel_freq = n / sum(n)) %>%    # относительные частоты
+  ungroup()
 
-# 4. преобразуйте в широкий формат; 
-# столбец c именем спикера превратите в имя ряда, используя подходящую функцию 
-friends_tf_wide <- friends_tf |> 
-  # ваш код здесь
+# 4. преобразуем в широкий формат
+friends_tf_wide <- friends_tf %>%
+  select(speaker, word, rel_freq) %>%
+  pivot_wider(names_from = word, values_from = rel_freq, values_fill = 0) %>%
+  column_to_rownames("speaker")        # превращаем столбец speaker в имена рядов
 
-# 5. установите зерно 123
-# проведите кластеризацию k-means (k = 3) на относительных значениях частотности (nstart = 20)
-# используйте scale()
+# 5. кластеризация k-means (k=3)
+set.seed(123)
+km.out <- kmeans(scale(friends_tf_wide), centers = 3, nstart = 20)
 
-# ваш код здесь
-km.out <- # ваш код здесь
+# 6. PCA
+pca_fit <- prcomp(scale(friends_tf_wide), center = TRUE, scale. = TRUE)
 
+# 7. биплот с текстом, цвет = кластер, отобрать 20 наиболее значимых переменных
+q <- fviz_pca_biplot(
+  pca_fit, 
+  label = "var", 
+  habillage = km.out$cluster, 
+  geom.ind = "text",       # текстовые метки для наблюдений
+  select.var = list(cos2 = 20)  # 20 самых важных переменных по косинусу
+)
 
-# 6. примените к матрице метод главных компонент (prcomp)
-# центрируйте и стандартизируйте, использовав аргументы функции
-pca_fit <- # ваш код здесь
-
-# 7. Покажите наблюдения и переменные вместе (биплот)
-# в качестве геома используйте текст (=имя персонажа)
-# цветом закодируйте кластер, выделенный при помощи k-means
-# отберите 20 наиболее значимых переменных (по косинусу, см. документацию к функции)
-# сохраните график как переменную q
-
-q <- # ваш код здесь
+q
 
 
